d4c54d26d77aa8d77be695b79f5416a3f8f41a94
G V Pranav Sharma <pranav.23bey10032@vitbhopal.ac.in>
2025-09-16 12:52:30 +0530

    Data parsing via req.body

--- routes_practice/index.js
+++ routes_practice/index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const app = express();
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 })
 
 app.post('/tacos', (req, res) => {
-    console.log(req.body)
-    res.send("POST /tacos response")
+    console.log(req.body);
+    const { food, qty } = req.body;
+    res.send(`Okay! ${qty} ${food} comming right up!`)
 })
 
 app.listen(3000, () => {
