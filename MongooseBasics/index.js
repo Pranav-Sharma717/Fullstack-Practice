6da9f71f7d4f74db34f5106ef7e255545f099461
G V Pranav Sharma <pranav.23bey10032@vitbhopal.ac.in>
2025-10-08 22:53:17 +0530

    Mongoose model

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -6,3 +6,22 @@ db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function () {
     console.log('Connection open')
 })
+
+//Defining a schema
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
+
+//Making a collection in Mongo
+const Movie = mongoose.model('Movie', movieSchema);
+//New instance og a DB:
+const Endgame = new Movie({
+    title: 'Avengers Endgame',
+    year: 2019,
+    score: 8.4,
+    rating: 'PG13'
+});
