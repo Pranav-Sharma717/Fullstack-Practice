169272e772fd880a19adaace406f26fc0d438e25
G V Pranav Sharma <pranav.23bey10032@vitbhopal.ac.in>
2025-12-30 23:39:03 +0530

    Update operation done for farmstand

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -42,8 +42,9 @@ app.get('/products/:id/edit', async (req, res) => {
 })
 
 app.put('/products/:id', async (req, res) => { //Put requests are mainly used for getting full data from the redirection while patch is used to get the partial info
-    console.log(req.body)
-    res.send("PUT!!")
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${product._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
